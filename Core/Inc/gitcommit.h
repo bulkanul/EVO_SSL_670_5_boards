f4a1940f308e6bf352285e18e175a7fde1b0f630
bulkanul <anton.segnet@gmail.com>
2026-06-15 14:33:51 +0300

    fix for psu struct copy in lsonoff psu command

--- Core/Inc/gitcommit.h
+++ Core/Inc/gitcommit.h
@@ -1,2 +1,2 @@
 #define GIT_INFO_PRESENT
- static const char* GIT_INFO = "Version Information=[4056e46a8844c96341f7f844f2ae345eca43e4ed, (HEAD -> adding_psu_rs232, origin/adding_psu_rs232)]\r\n";
+ static const char* GIT_INFO = "Version Information=[53abbb77c94e173e81026594ed5c3d626c712c6f, (HEAD -> adding_psu_rs232, origin/adding_psu_rs232)]\r\n";
