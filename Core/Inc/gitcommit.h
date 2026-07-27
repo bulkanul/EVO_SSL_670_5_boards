a23a28f1fb1cb0f25d233d9b79120d50827a37d7
bulkanul <anton.segnet@gmail.com>
2026-07-27 13:08:45 +0300

    lib update

--- Core/Inc/gitcommit.h
+++ Core/Inc/gitcommit.h
@@ -1,2 +1,2 @@
 #define GIT_INFO_PRESENT
- static const char* GIT_INFO = "Version Information=[f65cf6756475753c63d7d1598ffe65f4ee3051a9, (HEAD -> adding_psu_rs232, origin/adding_psu_rs232)]\r\n";
+ static const char* GIT_INFO = "Version Information=[7148240e19058f940bcdc482b9e3ffb8ce7318b2, (HEAD -> adding_psu_rs232, origin/adding_psu_rs232)]\r\n";
